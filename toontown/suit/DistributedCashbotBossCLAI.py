a7a592330e0dcd5345e089b6a831fbc481a9e072
DevvyDoo <devinhall4@gmail.com>
2022-02-03 02:17:30 -0500

    separate cfo class for official crane league matches

--- toontown/suit/DistributedCashbotBossCLAI.py
+++ toontown/suit/DistributedCashbotBossCLAI.py
@@ -7,13 +7,3 @@ class DistributedCashbotBossCLAI(DistributedCashbotBossAI.DistributedCashbotBoss
 
     def __init__(self, air):
         DistributedCashbotBossAI.DistributedCashbotBossAI.__init__(self, air)
-
-    def enterPrepareBattleThree(self):
-        super(DistributedCashbotBossCLAI, self).enterPrepareBattleThree()
-
-        # Only the first two toons are considered active participants
-        if len(self.involvedToons) > 2:
-            for spec in self.involvedToons[2:]:
-                t = self.air.doId2do.get(spec)
-                if t:
-                    self.enableSpectator(t)
