8d26fbc0b2fb5d200eedee7ad10527c0e0644dfb
DevvyDoo <devinhall4@gmail.com>
2022-02-10 17:47:33 -0500

    15 min crane round limit

--- toontown/coghq/CraneLeagueGlobals.py
+++ toontown/coghq/CraneLeagueGlobals.py
@@ -56,7 +56,7 @@ class CFORuleset:
 
     def __init__(self):
         self.TIMER_MODE = True  # When true, the cfo is timed and ends when time is up, when false, acts as a stopwatch
-        self.TIMER_MODE_TIME_LIMIT = 60*10  # How many seconds do we give the CFO crane round if TIMER_MODE is active?
+        self.TIMER_MODE_TIME_LIMIT = 60*15  # How many seconds do we give the CFO crane round if TIMER_MODE is active?
 
         self.CFO_MAX_HP = 1500  # How much HP should the CFO have?
         self.CFO_STUN_THRESHOLD = 30  # How much damage should a goon do to stun?
