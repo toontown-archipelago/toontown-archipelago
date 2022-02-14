926ee73ba0798e5381f34a7973853ea54222033a
Lou <thetycoon90@gmail.com>
2022-02-14 19:30:26 +0400

    Revert Client-sided object releasing
    Add in Anesidora Comments
    
    Re-implementation of Client-sided object releasing
    
    Polish client-sided releasing
    
    aaa
    
    aeee
    
    bbbb
    
    tyring thing

--- toontown/coghq/DistributedCashbotBossSafe.py
+++ toontown/coghq/DistributedCashbotBossSafe.py
@@ -8,19 +8,35 @@ import DistributedCashbotBossObject
 from toontown.coghq import CraneLeagueGlobals
 
 class DistributedCashbotBossSafe(DistributedCashbotBossObject.DistributedCashbotBossObject):
+
+    """ This is a safe sitting around in the Cashbot CFO final battle
+    room.  It's used as a prop for toons to pick up and throw at the
+    CFO's head.  Also, the special safe with self.index == 0
+    represents the safe that the CFO uses to put on his own head as a
+    safety helmet from time to time. """
+    
     notify = DirectNotifyGlobal.directNotify.newCategory('DistributedCashbotBossSafe')
+    
     grabPos = (0, 0, -8.2)
+    
+    # What happens to the crane and its cable when this object is picked up?
     craneFrictionCoef = 0.2
     craneSlideSpeed = 11
     craneRotateSpeed = 16
+
+    # A safe remains under physical control of whichever client
+    # last dropped it, even after it stops moving.  This allows
+    # goons to push safes out of the way.
     wantsWatchDrift = 0
 
     def __init__(self, cr):
         DistributedCashbotBossObject.DistributedCashbotBossObject.__init__(self, cr)
         NodePath.__init__(self, 'object')
         self.index = None
+        
         self.flyToMagnetSfx = loader.loadSfx('phase_5/audio/sfx/TL_rake_throw_only.ogg')
         self.hitMagnetSfx = loader.loadSfx('phase_5/audio/sfx/AA_drop_safe.ogg')
+        # We want these sfx's to overlap just a smidge for effect.
         self.toMagnetSoundInterval = Parallel(SoundInterval(self.flyToMagnetSfx, duration=ToontownGlobals.CashbotBossToMagnetTime, node=self), Sequence(Wait(ToontownGlobals.CashbotBossToMagnetTime - 0.02), SoundInterval(self.hitMagnetSfx, duration=1.0, node=self)))
         self.hitFloorSfx = loader.loadSfx('phase_5/audio/sfx/AA_drop_bigweight_miss.ogg')
         self.hitFloorSoundInterval = SoundInterval(self.hitFloorSfx, node=self)
@@ -33,16 +49,27 @@ class DistributedCashbotBossSafe(DistributedCashbotBossObject.DistributedCashbot
         DistributedCashbotBossObject.DistributedCashbotBossObject.announceGenerate(self)
         self.name = 'safe-%s' % self.doId
         self.setName(self.name)
+        
         self.boss.safe.copyTo(self)
         self.shadow = self.find('**/shadow')
+        
         self.collisionNode.setName('safe')
         #cs = CollisionSphere(0, 0, 4, 4) #TTR Collisions
         cs = CollisionCapsule(0, 0, 4, 0, 0, 4, 4) #TTCC Collisions
         self.collisionNode.addSolid(cs)
+        
         if self.index == 0:
+            # If this is safe 0, it's the safe that the CFO uses when
+            # he wants to put on his own helmet.  This one can't be
+            # picked up by magnets, and it doesn't stick around for
+            # any length of time when it's knocked off his head--it
+            # just falls through the floor and resets.
+            
             self.collisionNode.setIntoCollideMask(ToontownGlobals.PieBitmask | OTPGlobals.WallBitmask)
             self.collisionNode.setFromCollideMask(ToontownGlobals.PieBitmask)
+            
         self.boss.safes[self.index] = self
+        
         self.setupPhysics('safe')
         self.resetToInitialPosition()
 
@@ -57,6 +84,9 @@ class DistributedCashbotBossSafe(DistributedCashbotBossObject.DistributedCashbot
         self.shadow.show()
 
     def getMinImpact(self):
+        # This method returns the minimum impact, in feet per second,
+        # with which the object should hit the boss before we bother
+        # to tell the server.
         if self.boss.heldObject:
             return self.boss.ruleset.MIN_DEHELMET_IMPACT
         else:
@@ -77,12 +107,18 @@ class DistributedCashbotBossSafe(DistributedCashbotBossObject.DistributedCashbot
         self.physicsObject.setVelocity(0, 0, 0)
 
     def fellOut(self):
+        # The safe fell out of the world.  Reset it back to its
+        # original position.
         self.deactivatePhysics()
         self.d_requestInitial()
-
+        
     def setIndex(self, index):
         self.index = index
 
+        
+ 
+    ##### Messages To/From The Server #####
+    
     def setObjectState(self, state, avId, craneId):
         if state == 'I':
             self.demand('Initial')
@@ -92,10 +128,17 @@ class DistributedCashbotBossSafe(DistributedCashbotBossObject.DistributedCashbot
     def d_requestInitial(self):
         self.sendUpdate('requestInitial')
 
+
+
+    ### FSM States ###
+    
     def enterInitial(self):
         self.resetToInitialPosition()
         self.showShadows()
+        
         if self.index == 0:
+            # The special "helmet-only" safe goes away completely when
+            # it's in Initial mode.
             self.stash()
 
     def exitInitial(self):
