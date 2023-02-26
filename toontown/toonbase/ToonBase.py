ee78ddc0586de6db0cca2cc056a9639d29ec0b42
DESKTOP-5L85L2I\ian <topduckofficial@outlook.com>
2023-02-26 06:16:16 -0600

    keybinds: added a 0 power throw key
    GO STUNNAS!

--- toontown/toonbase/ToonBase.py
+++ toontown/toonbase/ToonBase.py
@@ -149,8 +149,9 @@ class ToonBase(OTPBase.OTPBase):
         self.ACTION_BUTTON = 'delete'
         self.SCREENSHOT_KEY = 'f9'
         self.CRANE_GRAB_KEY = 'control'
-
         self.SPRINT = 'shift'
+        self.SECONDARY_ACTION = 'insert'
+
         self.reloadControls()
 
         self.WANT_FOV_EFFECTS = self.settings.getBool('game', 'fovEffects', True)
@@ -484,6 +485,7 @@ class ToonBase(OTPBase.OTPBase):
             self.ACTION_BUTTON = keymap.get("ACTION_BUTTON", self.ACTION_BUTTON)
             self.CRANE_GRAB_KEY = keymap.get('CRANE_GRAB_KEY', self.CRANE_GRAB_KEY)
             self.SPRINT = keymap.get("SPRINT_KEY", self.SPRINT)
+            self.SECONDARY_ACTION = keymap.get("SECONDARY_ACTION", self.SECONDARY_ACTION)
             ToontownGlobals.OptionsPageHotkey = keymap.get(
                 "OPTIONS-PAGE", ToontownGlobals.OptionsPageHotkey
             )
@@ -496,6 +498,7 @@ class ToonBase(OTPBase.OTPBase):
             self.ACTION_BUTTON = "delete"
             self.CRANE_GRAB_KEY = 'control'
             self.SPRINT = 'shift'
+            self.SECONDARY_ACTION = "insert"
 
         self.accept(self.SCREENSHOT_KEY, self.takeScreenShot)
 
