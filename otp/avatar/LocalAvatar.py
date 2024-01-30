7b41bc3b217f1d0ae70248a542c3e4c19b58857b
DevvyDont <devinhall4@gmail.com>
2024-01-30 05:43:55 -0500

    hack fix for zone interest updates in visgroups

--- otp/avatar/LocalAvatar.py
+++ otp/avatar/LocalAvatar.py
@@ -320,6 +320,10 @@ class LocalAvatar(DistributedAvatar.DistributedAvatar, DistributedSmoothNode.Dis
         walkControls.setWallBitMask(wallBitmask)
         walkControls.setFloorBitMask(floorBitmask)
         walkControls.initializeCollisions(self.cTrav, self, avatarRadius, floorOffset, reach)
+        walkControls.lifter.setInPattern("on-floor")
+        walkControls.lifter.setOutPattern("off-floor")
+        walkControls.setCollisionsActive(0)
+        walkControls.setCollisionsActive(1)
         walkControls.setAirborneHeightFunc(self.getAirborneHeight)
         self.controlManager.add(walkControls, 'walk')
         self.physControls = walkControls
