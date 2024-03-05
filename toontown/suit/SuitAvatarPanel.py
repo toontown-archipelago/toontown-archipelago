4c11b88740c572425acca618d59ee5e833434ff3
OttavianoTT <thattoonkid03@gmail.com>
2024-03-04 20:49:15 -0500

    Suit Detail Panel: Revamp
    You can now see the cog's current health with their max.
    Opening and Closing has a new animation.
    Panel is colored dependent on the cog's department.

--- toontown/suit/SuitAvatarPanel.py
+++ toontown/suit/SuitAvatarPanel.py
@@ -8,15 +8,23 @@ from toontown.battle import SuitBattleGlobals
 from toontown.toonbase import TTLocalizer
 from otp.avatar import AvatarPanel
 from toontown.friends import FriendsListPanel
+from toontown.suit import Suit
+from direct.interval.IntervalGlobal import *
+from direct.showbase import DirectObject
+from panda3d.core import *
 
-class SuitAvatarPanel(AvatarPanel.AvatarPanel):
+
+class SuitAvatarPanel(AvatarPanel.AvatarPanel, DirectObject.DirectObject):
     currentAvatarPanel = None
 
     def __init__(self, avatar):
         AvatarPanel.AvatarPanel.__init__(self, avatar, FriendsListPanel=FriendsListPanel)
         self.avName = avatar.getName()
+        self.avatr = avatar
         gui = loader.loadModel('phase_3.5/models/gui/suit_detail_panel')
-        self.frame = DirectFrame(geom=gui.find('**/avatar_panel'), geom_scale=0.21, geom_pos=(0, 0, 0.02), relief=None, pos=(-0.2348, 0, -0.475), parent=base.a2dTopRight)
+        gui.find('**/shadow').setTransparency(TransparencyAttrib.MAlpha)
+        gui.find('**/shadow').setColor(1, 1, 1, 0.4)
+        self.frame = DirectFrame(geom=gui.find('**/avatar_panel'), geom_scale=0.21, geom_color=Suit.Suit.medallionColors[avatar.dna.dept], geom_pos=(0, 0, 0.02), relief=None, pos=(-0.2348, 0, -0.475), parent=base.a2dTopRight)
         disabledImageColor = Vec4(1, 1, 1, 0.4)
         text0Color = Vec4(1, 1, 1, 1)
         text1Color = Vec4(0.5, 1, 0.5, 1)
@@ -40,8 +48,9 @@ class SuitAvatarPanel(AvatarPanel.AvatarPanel):
         relativelevel = avatar.getLevel()
         revives = avatar.getMaxSkeleRevives() + 1
         attributes = SuitBattleGlobals.SuitAttributes[avatar.getStyleName()]
-        maxHP = attributes['hp'][relativelevel]
-        self.hpLabel = DirectLabel(parent=self.frame, pos=(0.0125, 0, -0.15), relief=None, text=TTLocalizer.AvatarPanelCogHP % maxHP, text_font=avatar.getFont(), text_fg=Vec4(0, 0, 0, 1), text_pos=(0, 0), text_scale = 0.047, text_wordwrap = 7.5, text_shadow=(1, 1, 1, 1))
+        maxHP = avatar.maxHP
+        HP = avatar.currHP
+        self.hpLabel = DirectLabel(parent=self.frame, pos=(0.0125, 0, -0.15), relief=None, text=TTLocalizer.AvatarPanelCogHP % (HP, maxHP), text_font=avatar.getFont(), text_fg=Vec4(0, 0, 0, 1), text_pos=(0, 0), text_scale = 0.047, text_wordwrap = 7.5, text_shadow=(1, 1, 1, 1))
         dept = SuitDNA.getSuitDeptFullname(avatar.dna.name)
         if revives == 1:
             self.levelLabel = DirectLabel(parent=self.frame, pos=(0, 0, -0.1), relief=None, text=TTLocalizer.AvatarPanelCogLevel % level, text_font=avatar.getFont(), text_align=TextNode.ACenter, text_fg=Vec4(0, 0, 0, 1), text_pos=(0, 0), text_scale=0.05, text_wordwrap=8.0)
@@ -57,25 +66,52 @@ class SuitAvatarPanel(AvatarPanel.AvatarPanel):
         menuX = -0.05
         menuScale = 0.064
         base.localAvatar.obscureFriendsListButton(1)
+        #create a LerpScaleInterval that scales the frame from 0 to 1
+        self.scaleUpVisual = LerpScaleInterval(self.frame, 0.25, Vec3(1.2, 1.2, 1.2), Vec3(0, 0, 0), blendType='easeIn')
+        self.scaleUpToNormal = LerpScaleInterval(self.frame, 0.15, Vec3(1, 1, 1), Vec3(1.2, 1.2, 1.2), blendType='easeInOut')
+        self.scaleUpInterval = Sequence(self.scaleUpVisual, self.scaleUpToNormal)
+        self.scaleUpInterval.start()
         self.frame.show()
+        self.wantClose = False
         messenger.send('avPanelDone')
         return
 
     def cleanup(self):
-        if self.frame == None:
-            return
-        self.frame.destroy()
-        del self.frame
-        self.frame = None
-        self.head.removeNode()
-        del self.head
-        base.localAvatar.obscureFriendsListButton(-1)
-        AvatarPanel.AvatarPanel.cleanup(self)
+        if self.wantClose == True:
+            if self.frame == None:
+                return
+            self.frame.destroy()
+            del self.frame
+            self.frame = None
+            self.head.removeNode()
+            del self.head
+
+            del self.scaleUpVisual
+            del self.scaleUpToNormal
+            del self.scaleUpInterval
+            del self.scaleDownVisual
+            del self.scaleDownToNormal
+            del self.scaleDownInterval
+
+            base.localAvatar.obscureFriendsListButton(-1)
+            AvatarPanel.AvatarPanel.cleanup(self)
+            self.panelNoneFunc()
+        return
+    
+    def panelNoneFunc(self):
+        AvatarPanel.currentAvatarPanel = None
+        return
+    
+    def setWantClose(self, wantClose):
+        self.wantClose = wantClose
         return
 
     def __handleClose(self):
-        self.cleanup()
-        AvatarPanel.currentAvatarPanel = None
+        self.scaleUpInterval.finish()
+        self.scaleDownVisual = LerpScaleInterval(self.frame, 0.25, Vec3(1.2, 1.2, 1.2), Vec3(1, 1, 1), blendType='easeIn')
+        self.scaleDownToNormal = LerpScaleInterval(self.frame, 0.15, Vec3(0, 0, 0), Vec3(1.2, 1.2, 1.2), blendType='easeInOut')
+        self.scaleDownInterval = Sequence(self.scaleDownVisual, self.scaleDownToNormal, Func(self.setWantClose, True), Func(self.cleanup), Func(self.setWantClose, False))
+        self.scaleDownInterval.start()
         return
 
     @classmethod
