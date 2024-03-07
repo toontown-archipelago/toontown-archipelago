bb4dae195b9b64c82f8bf6606beb8e973758d5f9
PIEisFANTASTIC <49802408+PIEisFANTASTIC@users.noreply.github.com>
2024-03-07 16:21:04 -0500

    Cogs: Max quotas 10 -> 5

--- toontown/shtiker/CogPageGlobals.py
+++ toontown/shtiker/CogPageGlobals.py
@@ -1,4 +1,4 @@
-COG_QUOTAS = ((1, 1, 1, 1, 1, 1, 1, 1), (10, 10, 10, 10, 10, 10, 10, 10))
+COG_QUOTAS = ((1, 1, 1, 1, 1, 1, 1, 1), (5, 5, 5, 5, 5, 5, 5, 5))
 COG_UNSEEN = 1
 COG_BATTLED = 2
 COG_DEFEATED = 3
