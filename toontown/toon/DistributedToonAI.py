a7445cfab7aa09a98cc75e7dbc0ca6d69f687b0a
DevvyDont <devinhall4@gmail.com>
2024-03-07 03:11:31 -0500

    remove money earn debug whispers

--- toontown/toon/DistributedToonAI.py
+++ toontown/toon/DistributedToonAI.py
@@ -2365,9 +2365,6 @@ class DistributedToonAI(DistributedPlayerAI.DistributedPlayerAI, DistributedSmoo
             bankMoney = self.bankMoney + overflowMoney
             self.b_setBankMoney(bankMoney)
 
-        # DEBUG
-        self.d_setSystemMessage(0, f"DEBUG: Received {deltaMoney} beans")
-
     def takeMoney(self, deltaMoney, bUseBank=True):
         totalMoney = self.money
         if bUseBank:
