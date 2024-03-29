d98c456a68d7281d1880077b5a8c2ad302a6e1de
DevvyDont <devinhall4@gmail.com>
2024-03-29 00:20:35 -0400

    oops

--- toontown/battle/DistributedBattle.py
+++ toontown/battle/DistributedBattle.py
@@ -174,7 +174,6 @@ class DistributedBattle(DistributedBattleBase.DistributedBattleBase):
         self._removeMembersKeep()
 
     def enterReward(self, ts):
-        super().enterReward()
         self.notify.debug('enterReward()')
         self.disableCollision()
         self.delayDeleteMembers()
