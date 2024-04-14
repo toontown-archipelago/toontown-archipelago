2312afaf768f31321b20fe6d1b1cdcf895a7b57f
Snowy <165027781+snowytt2@users.noreply.github.com>
2024-04-14 03:06:37 +0100

    Build: Introduce server start up script

--- build/scripts/start_all.bat
+++ build/scripts/start_all.bat
@@ -1,16 +1,8 @@
 @echo off
-title Toontown Archipelago - UberDOG Server
+title Toontown Archipelago - Launcher
 
 echo Starting Astron
-start start_astron.bat
-
-TIMEOUT /T 2
-echo Starting UberDOG
-start start_server_ud.bat
-
-TIMEOUT /T 2
-echo Starting AI
-start start_server_ai.bat
+start start_servers.bat
 
 TIMEOUT /T 2
 echo Starting Client
