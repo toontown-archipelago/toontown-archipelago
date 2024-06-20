6b6e000179d23d1c61ac2472417954bcef73ee01
DarthM <mgracer48@yahoo.com>
2024-06-19 20:44:09 -0400

    Racing: Racing controls now work again

--- toontown/racing/DistributedVehicle.py
+++ toontown/racing/DistributedVehicle.py
@@ -988,22 +988,26 @@ class DistributedVehicle(DistributedSmoothNode.DistributedSmoothNode, Kart.Kart,
     def enableControls(self):
         self.canRace = True
         self.accept(base.controls.JUMP, self.__controlPressed)
-        self.accept('control-up', self.__controlReleased)
+        self.accept(base.controls.JUMP + '-up', self.__controlReleased)
+        base.localAvatar.enableCraneControls()
         self.accept('InputState-forward', self.__upArrow)
         self.accept('InputState-reverse', self.__downArrow)
         self.accept('InputState-turnLeft', self.__leftArrow)
         self.accept('InputState-turnRight', self.__rightArrow)
+        self.arrowVert = 0
+        self.arrowHorz = 0
 
     def disableControls(self):
         self.arrowVert = 0
         self.arrowHorz = 0
-        self.ignore(base.controls.JUMP)
+        self.ignore(f'{base.controls.JUMP}-up')
         self.ignore('control-up')
         self.ignore('tab')
         self.ignore('InputState-forward')
         self.ignore('InputState-reverse')
         self.ignore('InputState-turnLeft')
         self.ignore('InputState-turnRight')
+        base.localAvatar.disableCraneControls()
 
     def setInput(self, on):
         if localAvatar.doId == self.ownerId:
