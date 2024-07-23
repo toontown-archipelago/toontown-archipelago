ea06f7a0038bd94eadf2cf9c1373e85a3f32e4df
Wizzerinus <development@toontowneventhorizon.com>
2024-07-23 16:43:15 +0300

    Build: That's not how Tar binary works

--- apworld/make_apworld.sh
+++ apworld/make_apworld.sh
@@ -3,7 +3,5 @@
 rm -f toontown.apworld
 # delete toontown.zip
 rm -f toontown.zip
-# archive up the toontown folder with tar with unix equivalent of tar -a -c -f toontown.zip toontown
-tar -acf toontown.zip toontown
-# rename toontown.zip to toontown.apworld
-mv toontown.zip toontown.apworld
+# make the archive
+zip -r toontown.apworld toontown
