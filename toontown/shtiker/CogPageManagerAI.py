e8c98bb1f4fb92261647954ba1073442294fba77
OttavianoTT <thattoonkid03@gmail.com>
2024-07-23 16:32:43 -0400

    custom cog pipeline: add 1 new cog to each department for showcase

--- toontown/shtiker/CogPageManagerAI.py
+++ toontown/shtiker/CogPageManagerAI.py
@@ -31,29 +31,33 @@ class CogPageManagerAI:
                 continue
 
             if toon.getDoId() in suit['activeToons']:
-
-                # AP location check
-                cog_location_unique_name = cog_code_to_ap_location(suit['type'])[0]
-                location_id = ap_location_name_to_id(cog_location_unique_name)
-                if location_id > 0:
-                    toon.addCheckedLocation(location_id)
-
-                suitIndex = SuitDNA.suitHeadTypes.index(suit['type'])
-                suitDept = SuitDNA.suitDepts.index(suit['track'])
-                cogQuota = get_min_cog_quota(toon)
-                buildingQuota = get_max_cog_quota(toon)
-
-                cogCount[suitIndex] += 1
-                cogStatus[suitIndex] = COG_DEFEATED
-
-                if cogQuota <= cogCount[suitIndex] < buildingQuota:
-                    cogStatus[suitIndex] = COG_COMPLETE1
-                else:
-                    cogStatus[suitIndex] = COG_COMPLETE2
-                    cog_location_unique_name = cog_code_to_ap_location(suit['type'])[1]
+                
+                # ik this is hacky, but it works for now with custom cogs as we dont have extra room for cog gallery
+                try:
+                    # AP location check
+                    cog_location_unique_name = cog_code_to_ap_location(suit['type'])[0]
                     location_id = ap_location_name_to_id(cog_location_unique_name)
                     if location_id > 0:
                         toon.addCheckedLocation(location_id)
+
+                    suitIndex = SuitDNA.suitHeadTypes.index(suit['type'])
+                    suitDept = SuitDNA.suitDepts.index(suit['track'])
+                    cogQuota = get_min_cog_quota(toon)
+                    buildingQuota = get_max_cog_quota(toon)
+
+                    cogCount[suitIndex] += 1
+                    cogStatus[suitIndex] = COG_DEFEATED
+
+                    if cogQuota <= cogCount[suitIndex] < buildingQuota:
+                        cogStatus[suitIndex] = COG_COMPLETE1
+                    else:
+                        cogStatus[suitIndex] = COG_COMPLETE2
+                        cog_location_unique_name = cog_code_to_ap_location(suit['type'])[1]
+                        location_id = ap_location_name_to_id(cog_location_unique_name)
+                        if location_id > 0:
+                            toon.addCheckedLocation(location_id)
+                except:
+                    pass
 
         toon.b_setCogStatus(cogStatus)
         toon.b_setCogCount(cogCount)
