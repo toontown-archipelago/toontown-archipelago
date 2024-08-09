5554b1eaf85744f56d49cb50ff4a0f0f9d41205b
Hidden Kitten <30309397+HiddenKitten@users.noreply.github.com>
2024-08-08 23:39:14 -0500

    Multitoon: Sync checked locations.
    Also happens to fix problems caused by connecting with a new toon to an existing slot, which might be necessary if a toon gets corrupted somehow.

--- toontown/archipelago/packets/clientbound/connected_packet.py
+++ toontown/archipelago/packets/clientbound/connected_packet.py
@@ -152,9 +152,13 @@ class ConnectedPacket(ClientBoundPacketBase):
 
         client.av.b_setName(client.slot_name)
 
-        # Is this this toon's first time? If so reset the toon's stats and initialize their settings from their YAML
+        # Is this this slot's first toon? If so reset the toon's stats and initialize their settings from their YAML
         if len(self.checked_locations) == 0:
             self.handle_first_time_player(client.av)
+        # Is this this specific toon's first slot? if so, reset this toon's stats and initialize from YAML. 
+        if len(client.av.checkedLocations) == 0:
+            self.handle_first_time_player(client.av)
+        
 
         self.debug(f"Detected slot data: {self.slot_data}")
         client.av.b_setSlotData(self.slot_data)
@@ -166,6 +170,9 @@ class ConnectedPacket(ClientBoundPacketBase):
         toonCheckedLocations = client.av.getCheckedLocations()
         if len(toonCheckedLocations) > 0:
             client.av.archipelago_session.sync()
+
+        # Receive all checks that were collected from our slot while disconnected
+        client.av.addCheckedLocations(self.checked_locations)
 
         # Tell AP we are playing
         won_id = ap_location_name_to_id(locations.ToontownLocationName.SAVED_TOONTOWN.value)
