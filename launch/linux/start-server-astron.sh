ec6b305fdb1cf5a91b12976296c34cb0ccf2347b
Michael G <mgracer48@yahoo.com>
2024-10-12 18:21:35 -0400

    Add astron file to support arm64 linux

--- launch/linux/start-server-astron.sh
+++ launch/linux/start-server-astron.sh
@@ -2,5 +2,11 @@
 echo "Toontown Archipelago: Astron Launcher"
 echo
 cd ../../astron
+# if architecture is arm64
+if [ "$(uname -m)" = "aarch64" ]; then
+    ./astrond-linux-arm64 --loglevel info config/astrond.yml
+else
+    ./astrond-linux --loglevel info config/astrond.yml
+fi
 
-./astrond-linux --loglevel info config/astrond.yml
+
