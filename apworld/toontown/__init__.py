c4a0b4c1d3ad729bad671b180692906a9ae6d2b8
PIEisFANTASTIC <49802408+PIEisFANTASTIC@users.noreply.github.com>
2025-07-15 21:35:13 -0400

    Version Bump

--- apworld/toontown/__init__.py
+++ apworld/toontown/__init__.py
@@ -574,7 +574,7 @@ class ToontownWorld(World):
         return {
             "seed": self.multiworld.seed,
             "team": self.options.team.value,
-            "game_version": "v0.16.1",
+            "game_version": "v0.17.0",
             "seed_generation_type": self.options.seed_generation_type.value,
             "starting_laff": self.options.starting_laff.value,
             "max_laff": self.options.max_laff.value,
