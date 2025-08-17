f7d1adfb5d780815a9e47c551c23465b63ab7e44
PIEisFANTASTIC <49802408+PIEisFANTASTIC@users.noreply.github.com>
2025-08-17 04:48:24 -0400

    version bump

--- toontown/toonbase/ToontownGlobals.py
+++ toontown/toonbase/ToontownGlobals.py
@@ -6,7 +6,7 @@ from direct.showbase.PythonUtil import invertDict
 from panda3d.core import BitMask32, Vec4, Filename
 import collections
 
-GameVersion = "v0.17.2"
+GameVersion = "v0.17.3"
 MapHotkey = 'MapHotkey'
 AccountDatabaseChannelId = 4008
 ToonDatabaseChannelId = 4021
