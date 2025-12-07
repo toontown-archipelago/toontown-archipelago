8dfd793b82d8a9e7c8a2bf2cef12ff58c0bf2646
PIEisFANTASTIC <49802408+PIEisFANTASTIC@users.noreply.github.com>
2025-12-07 02:29:18 -0500

    version bump

--- apworld/toontown/__init__.py
+++ apworld/toontown/__init__.py
@@ -660,7 +660,7 @@ class ToontownWorld(World):
         return {
             "seed": self.multiworld.seed,
             "team": self.options.team.value,
-            "game_version": "v0.18.2",
+            "game_version": "v0.18.4",
             "seed_generation_type": self.options.seed_generation_type.value,
             "starting_laff": self.options.starting_laff.value,
             "max_laff": self.options.max_laff.value,
--- config/common.prc
+++ config/common.prc
@@ -1,5 +1,5 @@
 # Server settings
-version v0.18.3 Alpha
+version v0.18.4 Alpha
 
 
 server-version tt-ap-edition
