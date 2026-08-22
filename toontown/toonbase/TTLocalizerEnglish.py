b856193316e2720d0baa0d0ed4c2b60ef6f91195
PIEisFANTASTIC <49802408+PIEisFANTASTIC@users.noreply.github.com>
2026-08-22 16:28:01 -0400

    Settings: Better clarify setting name for randomized music style

--- toontown/toonbase/TTLocalizerEnglish.py
+++ toontown/toonbase/TTLocalizerEnglish.py
@@ -10020,7 +10020,7 @@ OptionNames = {
     "sfx-volume": "Sound Effects Volume",
     "toon-chat-sounds": "Toon Chat Sounds",
     "random-music": "Randomize Music",
-    "random-music-style": "Music Selection",
+    "random-music-style": "Randomized Music Mode",
     'ap-sounds': "AP Item Sounds",
     'refresh-audio': "Refresh Audio",
 }
