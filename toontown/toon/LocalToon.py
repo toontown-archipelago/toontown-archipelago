9ff483af14aeb11835c0d6880f667b9d1b9b22bc
PIEisFANTASTIC <49802408+PIEisFANTASTIC@users.noreply.github.com>
2026-09-12 20:26:26 -0400

    Bugfix: Stop skill text popups when resetting toon that had a garden

--- toontown/toon/LocalToon.py
+++ toontown/toon/LocalToon.py
@@ -1535,6 +1535,7 @@ class LocalToon(DistributedToon.DistributedToon, LocalAvatar.LocalAvatar):
         self.__shovelButtonFake.hide()
 
     def levelWater(self, change = 1):
+        return
         if change < 0:
             return
         self.showWateringCanButtonFake(1)
@@ -1546,6 +1547,7 @@ class LocalToon(DistributedToon.DistributedToon, LocalAvatar.LocalAvatar):
         self.waterTrack.start()
 
     def levelShovel(self, change = 1):
+        return
         if change < 1:
             return
         self.showShovelButtonFake(1)
